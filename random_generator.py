fe7dae5c3bb363f7623e9c1ef000eb3e9b682568
nelliel <arch7nelliel@gmail.com>
2020-04-02 17:00:34 +0430

    changed a variable name to something more suitable

--- random_generator.py
+++ random_generator.py
@@ -9,14 +9,14 @@ def random_verse(poet):
     poems = select_poems.fetchall()
 
     id = []
-    for each_tuple in poems:
-        if each_tuple[3] == '':
+    for each_row in poems:
+        if each_row[3] == '':
             continue
 
-        url_feild = each_tuple[3]
+        url_feild = each_row[3]
         poet_name = url_feild.split('/')[3]
         if poet == poet_name:
-            id.append(each_tuple[0])
+            id.append(each_row[0])
     
     id_lenght = len(id)
     random_poemID =  randint(0, id_lenght)
